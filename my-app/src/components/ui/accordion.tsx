eac3e08ab9caf6c70a27502e0f753b794d7457c5
Dasun Sathsara <130266928+Dasun110@users.noreply.github.com>
2025-07-17 23:38:16 +0530

    create button

--- my-app/src/components/ui/accordion.tsx
+++ my-app/src/components/ui/accordion.tsx
@@ -5,7 +5,7 @@ import * as AccordionPrimitive from "@radix-ui/react-accordion";
 import { ChevronDownIcon } from "lucide-react";
 
 
-import { cn } from "@/utils";
+import { cn } from "./utils";
 
 function Accordion({
   ...props
